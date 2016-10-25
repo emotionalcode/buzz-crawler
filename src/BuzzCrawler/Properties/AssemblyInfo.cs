e10108537b9aa818fcd67eda3ccf4e335a40241c
DaeYoung Kim <dykim@live.com>
2016-10-25 17:23:37 +0900

    for nuget package

--- src/BuzzCrawler/Properties/AssemblyInfo.cs
+++ src/BuzzCrawler/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("BuzzCrawler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("buzz crawler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("emotionalcode")]
 [assembly: AssemblyProduct("BuzzCrawler")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
